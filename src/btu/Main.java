f0cede6ec89f3c174bbc923c00f8496bd4cca5c1
student <student@BTU.local>
2019-09-18 12:47:53 +0400

    first test

---
+++ src/btu/Main.java
@@ -0,0 +1,12 @@
+package btu;
+
+public class Main {
+
+    public static void Main(String[] args) {
+//	    System.out.println("Java");
+//        System.out.println("C++");
+//        System.out.println("C#");
+        int x = 12;
+        System.out.println(x+10);
+    }
+}
